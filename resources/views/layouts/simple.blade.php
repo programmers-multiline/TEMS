985d7342149e74a554f8a81bb506b7bb323bb0c5
Jhon Paolo Gaita <jhonpaologaita16@gmail.com>
2024-05-17 17:32:03 +0800

    magic

--- resources/views/layouts/simple.blade.php
+++ resources/views/layouts/simple.blade.php
@@ -20,15 +20,15 @@
   <meta property="og:image" content="">
 
   <!-- Icons -->
-  <link rel="shortcut icon" href="{{ asset('media/favicons/favicon.png') }}">
-  <link rel="icon" sizes="192x192" type="image/png" href="{{ asset('media/favicons/favicon-192x192.png') }}">
-  <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('media/favicons/apple-touch-icon-180x180.png') }}">
+  <link rel="shortcut icon" href="{{ asset('public/media/favicons/favicon.png') }}">
+  <link rel="icon" sizes="192x192" type="image/png" href="{{ asset('public/media/favicons/favicon-192x192.png') }}">
+  <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('public/media/favicons/apple-touch-icon-180x180.png') }}">
 
 
   <link rel="stylesheet" id="css-main" href="{{asset('js/codebase.min.css')}}">
   <!-- Modules -->
   @yield('css')
-  @vite(['resources/sass/main.scss', 'resources/js/codebase/app.js'])
+  @vite(['public/resources/sass/main.scss', 'public/resources/js/codebase/app.js'])
 
   <!-- Alternatively, you can also include a specific color theme after the main stylesheet to alter the default color theme of the template -->
   {{-- @vite(['resources/sass/main.scss', 'resources/sass/codebase/themes/corporate.scss', 'resources/js/codebase/app.js']) --}}
@@ -89,13 +89,13 @@
   </div>
   <!-- END Page Container -->
 
-  <script src="{{asset('js/codebase.app.min.js')}}"></script>
+  <script src="{{asset('public/js/codebase.app.min.js')}}"></script>
 
     <!-- jQuery (required for BS Notify plugin) -->
-    <script src="{{asset('js/lib/jquery.min.js')}}"></script>
+    <script src="{{asset('public/js/lib/jquery.min.js')}}"></script>
 
     <!-- Page JS Plugins -->
-    <script src="{{asset('js/plugins/bootstrap-notify/bootstrap-notify.min.js')}}"></script>
+    <script src="{{asset('public/js/plugins/bootstrap-notify/bootstrap-notify.min.js')}}"></script>
 
     <!-- Page JS Helpers (BS Notify Plugin) -->
     <script>Codebase.helpersOnLoad(['jq-notify']);</script>
