56b410c9fb2d29b454461492f66ce655babf98af
Jhon Paolo Gaita <jhonpaologaita16@gmail.com>
2024-05-20 09:44:15 +0800

    magic sana gumana!!!!!!

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,17 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
-
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css', 
-                'resources/js/app.js',
-
-        ],
-            refresh: true,
-        }),
+                'resources/sass/main.scss',
+                'resources/css/app.css',
+                'resources/js/app.js'
+            ],
+            refresh: true
+        })
     ],
-    
-        },
-        {
-        publicDir: 'public',
-        }
-);
+    publicDir: 'public' // Specify the correct directory path here
+});
+
