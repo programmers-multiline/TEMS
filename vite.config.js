e0bd6b04a6148ef100390a2bf0635ba6dc0bd40b
Jhon Paolo Gaita <jhonpaologaita16@gmail.com>
2024-05-20 08:56:10 +0800

    gumana kana!!

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
+
 export default defineConfig({
     plugins: [
         laravel({
@@ -12,4 +13,9 @@ export default defineConfig({
             refresh: true,
         }),
     ],
-});
+    
+        },
+        {
+        publicDir: 'public',
+        }
+);
