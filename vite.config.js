66a84e23a9ba1ac8e8137b691ac3aa997dff0899
Jhon Paolo Gaita <jhonpaologaita16@gmail.com>
2024-05-20 10:49:43 +0800

    a

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,22 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import sass from 'vite-plugin-sass'; // Import the sass plugin directly
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/sass/main.scss',
                 'resources/css/app.css',
                 'resources/js/app.js',
                 'resources/js/codebase/app.js',
             ],
             refresh: true
-        })
+        }),
+        sass({
+            input: [
+                'resources/sass/main.scss',
+            ]
+        }), // Include the sass plugin here
     ],
     // publicDir: 'public' // Specify the correct directory path here
 });
-
