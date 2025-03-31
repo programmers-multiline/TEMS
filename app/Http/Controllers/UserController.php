0f495a973ab01757aa58a20bf81ca42efc82a746
jdojade <100562473+jdojade@users.noreply.github.com>
2025-03-31 15:56:00 +0800

    login intergration from oms

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,18 +3,23 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Str;
+
 class UserController extends Controller
 {
-    public function auth_login(Request $request){
+    public function auth_login(Request $request)
+    {
 
 
         // if(!$user){
         //     notyf()->position('y', 'top')->addError('Invalid Credential.');
         //     return redirect()->back()->withInput($request->input());
         // }
-    
+
         // if ($user && $user->password === $request->login_password) {
         //     Auth::login($user);
         //     if($user->user_type_id == 1){
@@ -46,7 +51,6 @@ class UserController extends Controller
         Auth::login($user);
 
         return redirect()->intended('dashboard');
-
     }
 
 
@@ -58,5 +62,62 @@ class UserController extends Controller
         return redirect('/');
     }
 
-    
+    public function loginViaOMS(Request $request)
+    {
+        Log::info('Incoming request login data:', $request->all());
+
+        $data = $request->input('data');
+
+        $user = User::where('id', $data['id'])->first();
+
+        if ($user) {
+            $token = Str::random(60);
+
+            $user->login_token = $token;
+            $user->token_expires_at = Carbon::now()->addMinutes(15);
+            $user->save();
+
+            // Generate Link for login
+            $link = url('/login-with-token?token=' . $token . '&id=' . $user->id);
+            return response()->json([
+                'message' => 'User found, login link generated',
+                'login_link' => $link,
+            ], 201);
+        } else {
+            return response()->json(['message' => 'User not found'], 404);
+        }
+    }
+
+    public function authViaOMS(Request $request)
+    {
+        $token = $request->query('token');
+        $id = $request->query('id');
+
+        Log::info([$token, $id]);
+
+        if (!$token) {
+            return redirect('/');
+        }
+
+        $user = User::where('id', $id)
+            ->where('login_token', $token)
+            ->where('token_expires_at', '>', Carbon::now())
+            ->first();
+
+        Log::info($user);
+
+        if ($user) {
+            Auth::login($user);
+            // Invalidate the token
+            $user->login_token = null;
+            $user->token_expires_at = null;
+            $user->save();
+            //Generate Session
+            $request->session()->regenerate();
+
+            return redirect()->intended('dashboard');
+        }
+
+        return response('Invalid or expired token.', 404);
+    }
 }
---
+++ config/env.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'tems_login_secret_key' => 'n2WmzSJ8dDfsxzozPVkjQpE6Ayc4Bkf0',
+];
